7b84f129a37976e0b48ebcea60a897c10f6ef04e
er-nutella <71012600+er-nutella@users.noreply.github.com>
2020-09-18 16:02:11 +0200

    prova
    fai te campione

--- Platform/Assets/SwitchGravity.cs
+++ Platform/Assets/SwitchGravity.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class SwitchGravity : MonoBehaviour
 {
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag=="Switch")
+        {
+
+            if(Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                rb.gravityScale *= -1;
+            }
+        }
     }
 }
